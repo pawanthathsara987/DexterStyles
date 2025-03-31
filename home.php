8035fa91c9f7bfc23681762dfeab6c1305e7cc95
Pawan Thathsara <pawanthathsara987@gmail.com>
2025-03-31 11:40:54 +0530

    update home page and update CSS styling

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DexterStyles - Trendy Clothing for Every Style</title>
+    <link rel="stylesheet" href="./css/home.css">
+</head>
+<body>
+    <header>
+        <nav class="nav">
+            <div class="logo"><a href="./home.php"><img src="path-to-dexterstyles-logo.png" alt="DexterStyles Logo"></a></div>
+            <ul class="nav-menu">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#shop">Shop</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div class="nav-actions">
+                <a href="#search" class="search-icon"><img src="./img/search_icon.png" /></a>
+                <a href="#cart" class="cart-icon"><img src="./img/shopping_cart.png" /></a>
+                <button class="sign-in-btn" onclick="window.location.href='./pages/signin.php'">Sign In</button>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
